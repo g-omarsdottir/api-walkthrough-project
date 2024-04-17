0e9b40ce68d70802253815561dcd43d40324e597
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-04-17 15:45:01 +0000

    Add function to post form using POST method

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,18 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e=> postForm());
 
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch("API_URL", {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+            },
+        body: form,
+    })
+};
+
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
